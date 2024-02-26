61e73578ae392d1d7d95d8a9487b26956c6163f2
zoldyak <firman.akun1@gmail.com>
2024-02-26 16:03:01 +0700

    Upload di Github

---
+++ src/views/HomeView.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+   <h1>Home</h1>
+   <PostList :posts="posts" />
+  </div>
+</template>
+<script>
+import PostList from "../components/PostList.vue";
+import { ref } from 'vue';
+export default {
+  name: 'HomeView',
+  component:{
+    PostList
+  },
+  setup(){
+    console.log('setup');
+    const posts= ref([
+      {
+        id:1,
+        title:"Judul 1",
+        body:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+      },
+      {
+        id:2,
+        title:"Judul 2",
+        body:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+      }
+    ])
+    return {
+      posts
+    }
+  },
+}
+</script>
