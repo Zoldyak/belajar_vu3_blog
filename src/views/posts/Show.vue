afebb29d8ddcf09bcccfcf051f1ffa97ff3db792
zoldyak <firman.akun1@gmail.com>
2024-02-29 22:11:40 +0700

    Membuat Fungsi untuk Memanggil Single Data Post solved

--- src/views/posts/Show.vue
+++ src/views/posts/Show.vue
@@ -1,25 +1,39 @@
-<template lang="">
-    <div v-if="error">
-        {{error}}
-    </div>
+<template>
     <div>
-        <h3>
-            Name : {{post.first_name}}
-        </h3>
-        <p>email : {{post.email}}</p>
+        <div v-if="error">
+            {{ error }}
+        </div>
+        <div v-if="post">
+            <h3>
+                Name : {{ post.first_name }}
+            </h3>
+            <p>email : {{ post.email }}</p>
+        </div>
+        <div v-else>
+            Loading...
+        </div>
     </div>
 </template>
+
 <script>
+import { ref, onMounted } from 'vue';
 import getPost from '@/composable/api_postdetail';
+
 export default {
-   props:['id'],
+   props: ['id'],
    setup(props) {
-    const { post,error,load} = getPost(props.id)
-    load()
-    return { post,error}
+    
+
+        const { post,error,load } = getPost(props.id);
+
+        onMounted(async () => {
+            await load()
+        });
+
+        return { post, error };
    }
 }
 </script>
-<style lang="">
-    
+
+<style>
 </style>
