f26f3738909c3c04936a267ed27c9fb4b0d93e1f
zoldyak <firman.akun1@gmail.com>
2024-03-16 22:55:59 +0700

    Konfigurasi Sdk Firebase Di Vue Cli

---
+++ src/firebase/config.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import 'firebase/firestore'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBDeVNAxFz-cu7sVRDRWkBp2dW_fRBm3dY",
+  authDomain: "belajarvue3blog.firebaseapp.com",
+  projectId: "belajarvue3blog",
+  storageBucket: "belajarvue3blog.appspot.com",
+  messagingSenderId: "172259573830",
+  appId: "1:172259573830:web:4ef38cc73355f6fa2e8a47"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const projectfirestore= firebase.firestore()
+
+export {projectfirestore}
