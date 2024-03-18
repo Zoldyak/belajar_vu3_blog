b765f2a0ec2b1039e29e32f475389d4436456837
zoldyak <firman.akun1@gmail.com>
2024-03-18 21:17:06 +0700

    error Menampilkan Seluruh Data Collection Firestore Dalam Vue

--- src/composable/api_pots.js
+++ src/composable/api_pots.js
@@ -1,5 +1,5 @@
 import { ErrorCodes, ref } from 'vue'
-
+import { projectfirestore } from "@/firebase/config";
 const getPosts = ()=>{
     const posts = ref([])
     const error =ref(null)
@@ -9,11 +9,21 @@ const getPosts = ()=>{
             console.log("LOADING...");
             setTimeout(resolve, 2000)
         })
-          let data = await  fetch('http://localhost:3000/posts')
-          if (!data.ok ) {
-            throw Error('Tidak Ada Data');
+
+        const res= await projectfirestore.collection('post').get()
+        const data= res.docs.map(()=>{
+          return  {
+            ...doc.data(),
+            id:doc.id
           }
-          posts.value = await data.json();
+        })
+        console.log(data);
+
+          // let data = await  fetch('http://localhost:3000/posts')
+          // if (!data.ok ) {
+          //   throw Error('Tidak Ada Data');
+          // }
+          // posts.value = await data.json();
         } catch (err) {
           error.value=err.message
         }
